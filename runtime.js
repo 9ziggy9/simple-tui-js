f9bb08e98d12981a6e9bfefadc6b3889d9173bad
9ziggy9 <davidarogers@protonmail.com>
2022-12-13 17:02:05 -0800

    Guessing game implemented

--- runtime.js
+++ runtime.js
@@ -1,11 +1,57 @@
+class GuessingGame {
+  constructor(max) {
+    this.answer = Math.ceil(max * Math.random());
+    this.range = max;
+    this.lastGuess = null;
+  }
+  checkAnswer = (guess) => {
+    if (guess === this.answer) {
+      console.log("\n");
+      console.log("You win!");
+      console.log(`You correctly guessed the answer of ${guess}`);
+      console.log("\n");
+      return false;
+    }
+    if (Math.abs(this.lastGuess - this.answer) > Math.abs(guess - this.answer)) {
+      console.log("Getting warmer... ");
+    }
+    if (Math.abs(this.lastGuess - this.answer) < Math.abs(guess - this.answer)) {
+      console.log("Getting colder... ");
+    }
+    this.lastGuess = guess;
+    return true;
+  };
+  greet = () => {
+    console.log(this.answer);
+    console.log("\n");
+    console.log("Welcome to the Guessing Game.");
+    console.log(`Guess a number between 1 and ${this.range}`);
+    console.log("\n");
+  };
+  quit = () => {
+    console.log("\n");
+    console.log("Thanks for playing Guessing Game!");
+    console.log("The answer was", this.answer);
+    console.log("Goodbye");
+    console.log("\n");
+    return false;
+  }
+}
+
 const inGreen = str => "\x1b[32m" + str + "\x1b[0m";
 
 async function commandHandler(cmd, prompt) {
   const [op, arg] = cmd.split(" ");
   switch(op) {
   case "guess":
-    console.log("TODO: make guessing game");
-    console.log("User provided argument: ", arg);
+    let game = new GuessingGame(arg ? Number(arg) : 10);
+    game.greet();
+    let subRunning = true;
+    while (subRunning) {
+      const guess = await prompt.question("--> ");
+      if (guess === "quit") subRunning = game.quit();
+      else subRunning = game.checkAnswer(Number(guess));
+    }
     return true;
   case "help":
     console.log("\n");
