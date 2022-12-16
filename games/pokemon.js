b786784e6145478e6baaf9988a6bba2953cdfdbf
9ziggy9 <davidarogers@protonmail.com>
2022-12-15 17:00:30 -0800

    implemented evolution

--- games/pokemon.js
+++ games/pokemon.js
@@ -9,17 +9,43 @@ class Pokemon {
     this.hunger = 0;
     this.hp = 100;
     this.isAlive = true;
-    this.evolutionStage = 0;
+    this.stage = -1;
     this.lifeline = setInterval(() => {
       this.hunger += this.hunger === 100 ? 0 : 10;
       if (this.hunger === 100) this.hp -= this.hp === 0 ? 0 : 10;
       if (this.hp === 0) {
 	this.die();
-	clearInterval(this.lifeline);
       }
     }, 5000);
   }
-  static feed = (pokemon) => pokemon.hunger -= pokemon.hunger === 0 ? 0 : 10;
+
+  static pokeInfo = {
+    "pikachu": ["raichu"],
+    "squirtle": ["blastoise"],
+    "charmander": ["charmeleon", "charizard"],
+    "bulbasaur": [],
+  };
+
+  eat = () => this.hunger -= this.hunger === 0 ? 0 : 10;
+
+  evolve = () => {
+    let newStage = this.stage + 1;
+    let oldPokemon = Object.entries(Pokemon.pokeInfo)
+	.map(pair => [pair[0], ...pair[1]])
+	.find(e => e.includes(this.name))[0];
+    let evolutions = Pokemon.pokeInfo[oldPokemon];
+    if (newStage > evolutions.length) {
+      console.log("Can't evolve!");
+    } else {
+      this.stage = newStage;
+      this.name = evolutions[newStage];
+      this.hp *= 2;
+      console.log("\n");
+      console.log(`${oldPokemon} evolved into ${this.name}`);
+      console.log("\n");
+    }
+  };
+
   die = () => this.isAlive = false;
   info = () => {
     if (this.isAlive) {
@@ -40,21 +66,50 @@ class PokeGame {
   constructor() {
     this.pokeList = [];
   }
+
+  takeToPokeCenter = () => {
+    this.pokeList.forEach(pokemon => {
+      pokemon.isAlive = true;
+      pokemon.hp = 100;
+      pokemon.hunger = 0;
+    });
+  }
+
+  killall = () => {
+    this.pokeList.forEach(pokemon => {
+      pokemon.isAlive = false;
+      pokemon.hp = 0;
+    });
+  }
+
   greet = () => {
     console.log("Hello!");
     console.log("Welcome to the Pokemon game");
   };
+
   catch = (pokemon) => {
     console.log(`\nYou got a wild ${pokemon}\n`);
     this.pokeList.push(new Pokemon(pokemon));
   };
+
   list = () => this.pokeList.forEach(pokemon => pokemon.info());
+
   subCommand = (cmd) => {
     const [op, arg] = cmd.split(" ");
     switch(op) {
+    case "killall":
+      this.killall();
+      return true;
+    case "pokecenter":
+      this.takeToPokeCenter();
+      return true;
+    case "evolve":
+      let evolvePokemon = this.pokeList.find((pokemon) => pokemon.name === arg);
+      evolvePokemon.evolve();
+      return true;
     case "feed":
       let selectedPokemon = this.pokeList.find((pokemon) => pokemon.name === arg);
-      Pokemon.feed(selectedPokemon);
+      selectedPokemon.eat();
       return true;
     case "help":
       console.log("\n");
@@ -67,6 +122,10 @@ class PokeGame {
       this.list();
       return true;
     case "catch":
+      if (!(Object.keys(Pokemon.pokeInfo).some(p => p === arg))) {
+	console.log("Pokemon doesn't exist OR has to be evolved to.");
+	return true;
+      }
       this.catch(arg);
       return true;
     case "quit":
